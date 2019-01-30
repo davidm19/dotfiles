007e7f34b1e534d22ab1d35a965ff2c6da683ac3
davidm19 <dmalone@chadwickschool.org>
2019-01-29 19:24:40 -0800

    Updating dotfiles...

--- vim/pathogen_update.sh
+++ vim/pathogen_update.sh
@@ -1,7 +1,6 @@
 DATE_CURRENTLY="$(date +"%u")-$(date +"%T")" #THESE TWO LINES GO IN BASHRC
-if [[ "$DATE_CURRENTLY" == "2-16:00:00" ]]; then #THESE TWO LINES GO IN BASHRC
-    # SO EVERY TUESDAY AT 4PM PLUGINS UPDATE
-    CURRENT_DIR=$(pwd)
+if [[ "$DATE_CURRENTLY" == "2-16:00:00" ]]; then #THESE TWO LINES GO IN BASHRC (SO EVERY TUESDAY AT 4PM PLUGINS UPDATE)
+    CURRENT_DIR=$(pwd) # THIS IS IN A SEPARATE SCRIPT
     cd ~/.vim/bundle/
     for dir in $(ls); do # GO INTO EACH DIRECTORY IN .vim/bundle AND GIT PULL
         cd $dir
